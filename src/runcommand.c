021caaf307cf4fc54bdc645d7723adf0b3dd8411
Paul Colomiets <paul@colomiets.name>
2011-07-28 01:03:42 +0300

    Added more process parameters

--- src/runcommand.c
+++ src/runcommand.c
@@ -1,14 +1,90 @@
-#include "runcommand.h"
-
+#define _GNU_SOURCE
 #include <unistd.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <alloca.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
+#include <pwd.h>
+#include <grp.h>
+
+#include "runcommand.h"
+
+#define CHECK(res, msg) if((res) < 0) { perror(msg); abort(); }
+
+static void open_files(config_process_t *process) {
+}
+
+static void set_limit(int resource, unsigned long value) {
+    struct rlimit limit = { value, value };
+    setrlimit(resource, &limit);
+}
+
+static void set_limits(config_process_t *process)
+{
+    if(process->limits.core)
+        set_limit(RLIMIT_CORE, process->limits.core);
+    if(process->limits.cpu)
+        set_limit(RLIMIT_CPU, process->limits.cpu);
+    if(process->limits.data)
+        set_limit(RLIMIT_DATA, process->limits.data);
+    if(process->limits.fsize)
+        set_limit(RLIMIT_FSIZE, process->limits.fsize);
+    if(process->limits.nofile)
+        set_limit(RLIMIT_NOFILE, process->limits.nofile);
+    if(process->limits.stack)
+        set_limit(RLIMIT_STACK, process->limits.stack);
+    if(process->limits.as)
+        set_limit(RLIMIT_AS, process->limits.as);
+}
+
+static void drop_privileges(config_process_t *process) {
+    if(getuid() > 0)
+        return;
+    if(process->group_len) {
+        char *end;
+        int gid = strtol(process->group, &end, 0);
+        if(end != process->group + process->group_len) {
+            struct group *gr = getgrnam(process->group);
+            CHECK(gr ? -1 : 0, "Can't find group");
+            gid = gr->gr_gid;
+        }
+        CHECK(setgid(gid), "Can't set gid");
+    }
+    if(process->user_len) {
+        char *end;
+        int uid = strtol(process->user, &end, 0);
+        if(end != process->user + process->user_len) {
+            struct passwd *pw = getpwnam(process->user);
+            CHECK(pw ? -1 : 0, "Can't find user");
+            uid = pw->pw_uid;
+        }
+        CHECK(setuid(uid), "Can't set uid");
+    }
+    if(process->chroot) {
+        CHECK(chroot(process->chroot), "Can't chroot");
+    }
+}
 
 int fork_and_run(config_process_t *process) {
     int res = fork();
-    assert(res >= 0);
+    if(res < 0)
+        return -1;  // Error occured
     if(res > 0)
-        return res;
+        return res; // We are parent, just return pid
+
+    open_files(process);
+    set_limits(process);
+    drop_privileges(process);
+
+    if(process->umask > 0) {
+        CHECK(umask(process->umask), "Can't set umask");
+    }
+    if(process->work_dir_len) {
+        CHECK(chdir(process->work_dir), "Can't set workdir");
+    }
+
     char *argv[process->arguments_len+1];
     char *environ[process->environ_len+1];
     int i = 0;
@@ -26,8 +102,8 @@ int fork_and_run(config_process_t *process) {
     }
     environ[i] = NULL;
 
-    res = execve(process->executable_path, argv, environ);
-    assert(res >= 0);  // which in turn cannot be true
+    CHECK(execve(process->executable_path, argv, environ),
+        "Can't execute");
     return 0;
 }
 
